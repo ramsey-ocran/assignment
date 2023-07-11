2866281ddfb9fdced063f26929f418c325b70c13
ramsey-ocran <ocranramsey1@gmail.com>
2023-07-11 11:15:53 +0000

    setup

---
+++ game.py
@@ -0,0 +1,18 @@
+# importing random module
+import random
+#  correct guess the number
+guess_number = random.randint(1,20)
+# i=0
+# while i<5:
+#  user_number= int(input('Enter guess number (1 - 20) : ') )
+#  if user_number==guess_number:
+#   print('correct')
+#  else:
+#     print('Guess is wrong')
+#  i=i+1
+# abc=random.randint(1,10)
+# print(abc)
+for i in range(10):
+    if i == 2:
+        break
+    print(i)
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Guessing Game
+- This is a guessing game to test my ability in python programming
+## Features of Guessing Game
+- User's are able to guess a number between certain range 
+- User's have number of attept to guess it guess number correct
+- User's receive prices when they win(MOMO, Credit)
